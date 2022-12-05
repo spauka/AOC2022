b93670baa72547461978a9df147a09d130d07da9
Sebastian Pauka <spauka@gmail.com>
2022-12-05 16:42:28 +1100

    D5 soln

---
+++ python/d5.py
@@ -0,0 +1,25 @@
+from utils import read_numbers
+
+piles = [[] for _ in range(9)]
+with open("input5.txt") as f:
+    inp = f.readlines()
+    instr = [read_numbers(x) for x in inp[10:]]
+
+    for line in inp[:8]:
+        for j, c in enumerate(zip(*[iter(line)]*4)):
+            if c[1] != " ":
+                piles[j].append(c[1])
+    for pile in piles:
+        pile.reverse()
+piles2 = [l[:] for l in piles]
+
+for c, f, t in instr:
+    for i in range(c):
+        piles[t-1].append(piles[f-1].pop())
+
+for c, f, t in instr:
+    piles2[t-1].extend(piles2[f-1][-c:])
+    piles2[f-1] = piles2[f-1][:-c]
+
+print("".join(pile[-1] for pile in piles))
+print("".join(pile[-1] for pile in piles2))
